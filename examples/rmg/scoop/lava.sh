599d78f1a2234a634146081414725f832b213d03
Richard West <r.west@neu.edu>
2013-03-27 17:58:53 -0400

    Added RMG_DB_FILE environment variable to lava.sh batch script

--- examples/rmg/scoop/lava.sh
+++ examples/rmg/scoop/lava.sh
@@ -54,5 +54,5 @@ done
 echo "Your lamboot hostfile looks like:" >> $LOGFILE
 cat $LAMHOST_FILE >> $LOGFILE
 
-
+export RMG_DB_FILE=$PWD/database.pkl
 python -m scoop -vv --hostfile $LAMHOST_FILE $RMGpy/rmg.py input.py > RMG.stdout.log
