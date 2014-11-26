4ecb385aa47622c23146abc1eb89485b0ed3357e
Belinda Slakman <slakman.b@husky.neu.edu>
2014-11-26 14:43:20 -0800

    Hard code behavior for H2 transfer family.
    Since it's its own reverse, we need to make sure the reactants and products match up, as in H-Abstraction.

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -1707,6 +1707,28 @@ class KineticsFamily(Database):
                     pairs.append([reaction.reactants[1],reaction.products[0]])
                 else:
                     error = True
+        elif self.label.lower() == 'h2_transfer':
+            # Hardcoding for H2 transfer: pair the reactant containing
+            # *1 with the product containing *4 and vice versa
+            assert len(reaction.reactants) == len(reaction.products) == 2
+            if reaction.reactants[0].containsLabeledAtom('*1'):
+                if reaction.products[0].containsLabeledAtom('*4'):
+                    pairs.append([reaction.reactants[0],reaction.products[0]])
+                    pairs.append([reaction.reactants[1],reaction.products[1]])
+                elif reaction.products[1].containsLabeledAtom('*4'):
+                    pairs.append([reaction.reactants[0],reaction.products[1]])
+                    pairs.append([reaction.reactants[1],reaction.products[0]])
+                else:
+                    error = True
+            elif reaction.reactants[1].containsLabeledAtom('*1'):
+                if reaction.products[1].containsLabeledAtom('*4'):
+                    pairs.append([reaction.reactants[0],reaction.products[0]])
+                    pairs.append([reaction.reactants[1],reaction.products[1]])
+                elif reaction.products[0].containsLabeledAtom('*4'):
+                    pairs.append([reaction.reactants[0],reaction.products[1]])
+                    pairs.append([reaction.reactants[1],reaction.products[0]])
+                else:
+                    error = True
         elif self.label.lower() == 'disproportionation':
             # Hardcoding for disproportionation: pair the reactant containing
             # *1 with the product containing *1
