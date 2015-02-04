e174abbf825e33b934b4c77edaa0a84c8e701270
Belinda Slakman <slakman.b@husky.neu.edu>
2015-02-04 11:18:07 -0500

    Add all libraries and families available to silicon hydride input file.
    Also added PDep and changed temperatures, pressures to more realistic values.
    Put SiH3 and H in core in order to encourage the radical chemistry.
    
    Conflicts:
    	examples/rmg/silane/input.py

--- examples/rmg/silane/input.py
+++ examples/rmg/silane/input.py
@@ -1,10 +1,10 @@
 # Data sources
 database(
-    thermoLibraries = ['primaryThermoLibrary'],
-    reactionLibraries = [('Silicon_Giunta_1990', False)],
+    thermoLibraries = ['SiliconHydrideLibrary', 'primaryThermoLibrary'],
+    reactionLibraries = [('Silicon_Giunta_1990', False), ('DolletSi2H4', False)],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
-    kineticsFamilies = 'none',
+    kineticsFamilies = ['Silylene_Insertion', 'Silylene_to_Silene', 'H_Abstraction', 'H2_transfer'],
     kineticsEstimator = 'rate rules',
 )
 
@@ -16,23 +16,37 @@ species(
 )
 
 species(
-    label='H2',
+    label='Ar',
+    reactive=False,
+    structure=SMILES("[Ar]")
+)
+
+species(
+    label='SiH3',
     reactive=True,
-    structure=SMILES("[H][H]")
+    structure=SMILES("[SiH3]")
+)
+
+species(
+    label='H',
+    reactive=True,
+    structure=SMILES("[H]")
 )
 
 # Reaction systems
 simpleReactor(
-    temperature=(800,'K'),
-    pressure=(1.0,'bar'),
+    temperature=(1000,'K'),
+    pressure=(0.5,'bar'),
     initialMoleFractions={
-        "SiH4": 0.9,
-	"H2": 0.1
+        "SiH4": 0.098,
+        "SiH3": 0.001,
+        "H": 0.001,
+	"Ar": 0.9
     },
     terminationConversion={
         'SiH4': 0.9,
     },
-    terminationTime=(1e6,'s'),
+    terminationTime=(1, 'hour')
 )
 
 simulator(
@@ -42,9 +56,18 @@ simulator(
 
 model(
     toleranceKeepInEdge=0.0,
-    toleranceMoveToCore=0.1,
+    toleranceMoveToCore=0.001,
     toleranceInterruptSimulation=0.1,
     maximumEdgeSpecies=100000
+)
+
+pressureDependence(
+    method='modified strong collision',
+    maximumGrainSize=(0.5,'kcal/mol'),
+    minimumNumberOfGrains=250,
+    temperatures=(300,2000,'K',8),
+    pressures=(0.01,20,'bar',5),
+    interpolation=('Chebyshev', 6, 4),
 )
 
 options(
@@ -53,5 +76,6 @@ options(
     drawMolecules=False,
     generatePlots=False,
     saveEdgeSpecies=True,
-    saveConcentrationProfiles=True,
 )
+
+
