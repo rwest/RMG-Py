2e9f2e0f7a787de7b32f84f682ef8f63b1b685fc
Richard West <r.west@neu.edu>
2015-05-22 16:52:05 -0400

    fixup! Changing some imports to help Anaconda build

--- rmgpy/rmg/output.py
+++ rmgpy/rmg/output.py
@@ -368,7 +368,7 @@ def saveDiffHTML(path, commonSpeciesList, speciesList1, speciesList2, commonReac
     This function outputs the species and reactions on an HTML page
     for the comparison of two RMG models.
     """
-    from rmgpy.model import PDepReaction
+    from rmgpy.rmg.model import PDepReaction
     from rmgpy.kinetics import Arrhenius, MultiArrhenius, MultiPDepArrhenius
 
     from rmgpy.molecule.draw import MoleculeDrawer
