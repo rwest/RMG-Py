3e1058e979676040b1bf3d7884bda76b7e90e7fa
Belinda Slakman <slakman.b@husky.neu.edu>
2015-10-08 17:18:03 -0400

    Update silane example for comparison with silane thermal decomposition experiment.

--- examples/rmg/silane/input.py
+++ examples/rmg/silane/input.py
@@ -4,7 +4,7 @@ database(
     reactionLibraries = [('Silicon_Giunta_1990', False), ('DolletSi2H4', False)],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
-    kineticsFamilies = ['Silylene_Insertion', 'Silylene_to_Silene', 'H_Abstraction', 'H2_transfer'],
+    kineticsFamilies = ['R_Recombination', 'Silylene_Insertion', 'Silylene_to_Silene', 'H_Abstraction', 'H2_transfer'],
     kineticsEstimator = 'rate rules',
 )
 
@@ -21,32 +21,18 @@ species(
     structure=SMILES("[Ar]")
 )
 
-species(
-    label='SiH3',
-    reactive=True,
-    structure=SMILES("[SiH3]")
-)
-
-species(
-    label='H',
-    reactive=True,
-    structure=SMILES("[H]")
-)
-
 # Reaction systems
 simpleReactor(
-    temperature=(1000,'K'),
-    pressure=(0.5,'bar'),
+    temperature=(913,'K'),
+    pressure=(39000,'Pa'),
     initialMoleFractions={
-        "SiH4": 0.098,
-        "SiH3": 0.001,
-        "H": 0.001,
-	"Ar": 0.9
+        "SiH4": 0.00016,
+	"Ar": 0.99984,
     },
     terminationConversion={
         'SiH4': 0.9,
     },
-    terminationTime=(1, 'hour')
+    terminationTime=(1, 's')
 )
 
 simulator(
@@ -56,26 +42,30 @@ simulator(
 
 model(
     toleranceKeepInEdge=0.0,
-    toleranceMoveToCore=0.001,
-    toleranceInterruptSimulation=0.1,
+    toleranceMoveToCore=0.1,
+    toleranceInterruptSimulation=0.5,
     maximumEdgeSpecies=100000
 )
 
-pressureDependence(
-    method='modified strong collision',
-    maximumGrainSize=(0.5,'kcal/mol'),
-    minimumNumberOfGrains=250,
-    temperatures=(300,2000,'K',8),
-    pressures=(0.01,20,'bar',5),
-    interpolation=('Chebyshev', 6, 4),
-)
+#pressureDependence(
+#    method='modified strong collision',
+#    maximumGrainSize=(0.5,'kcal/mol'),
+#    minimumNumberOfGrains=250,
+#    temperatures=(300,2000,'K',8),
+#    pressures=(0.01,20,'bar',5),
+#    interpolation=('Chebyshev', 6, 4),
+#)
 
 options(
     units='si',
     saveRestartPeriod=None,
-    drawMolecules=False,
+    drawMolecules=True,
     generatePlots=False,
     saveEdgeSpecies=True,
+)
+
+generatedSpeciesConstraints(
+    maximumSiliconAtoms=3
 )
 
 
